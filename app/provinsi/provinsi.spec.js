600e09e1503588be2e856a93bca83b0cc6a98f02
aldrichvalentino <aldrich.vh97@gmail.com>
2017-10-29 14:36:31 +0700

    added provinsi spec and user spec updated

--- app/provinsi/provinsi.spec.js
+++ app/provinsi/provinsi.spec.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-expressions */
 'use strict';
-
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinonChai = require('sinon-chai');
@@ -10,77 +9,82 @@ const expect = chai.expect;
 const routes = require('../app');
 const knex = require('../components/knex');
 
-describe('User handling', function () {
-  beforeEach((done) => {
-    knex.migrate.rollback()
+describe('provinsi handling', function () {
+  beforeEach(done => {
+    knex.migrate
+      .rollback()
       .then(() => knex.migrate.latest())
       .then(() => knex.seed.run())
       .then(() => done());
   });
 
-  after((done) => {
-    knex.migrate.rollback()
+  after(done => {
+    knex.migrate
+      .rollback()
       .then(() => knex.migrate.latest())
       .then(() => knex.seed.run())
       .then(() => done());
   });
 
-  describe('new user', function () {
-    let createNewUser = {
-      'username': 'raydreww',
-      'email': 'raydreww@gmail.com',
-      'password': 'hello123'
+  describe('Provinsi handling', function () {
+    let provinsi = {
+      username: 'test1'
     };
 
-    it('should return 201 after creating new user', (done) => {
-      chai.request(routes).post('/api/users').send(createNewUser).end((err, res) => {
-        expect(err).to.be.falsy;
-        expect(res).to.have.status(201);
-        expect(res).to.be.a('object');
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.haveOwnProperty('created_at');
-        expect(res.body.username).to.equal('raydreww');
-        done();
-      });
-    });
-
-    it('should not get list of users if user is not logged in', (done) => {
-      chai.request(routes).post('/api/users').send(createNewUser).end((err, res) => {
-        expect(err).to.be.falsy;
-        chai.request(routes).get('/api/users/' + createNewUser.username).end((err, resfromget) => {
-          expect(err).to.be.falsy;
+    // get/provinsi
+    it('should not get list of provinsi if provinsi or higher is not logged in', done => {
+      chai
+        .request(routes)
+        .get('/api/provinsi')
+        .end((err, resfromget) => {
+          expect(err);
           expect(resfromget).to.have.status(401);
           expect(resfromget.body.message).to.equal('Unauthorized');
           expect(resfromget.body.name).to.equal('UnauthorizedError');
           done();
         });
-      });
     });
 
-    it('should not delete user if user is not logged in', (done) => {
-      chai.request(routes).post('/api/users').send(createNewUser).end((err, res) => {
-        expect(err).to.be.falsy;
-        chai.request(routes).delete('/api/users/' + createNewUser.username).end((err, resfromdel) => {
-          expect(err).to.be.falsy;
-          expect(resfromdel).to.have.status(401);
-          expect(resfromdel.body.message).to.equal('Unauthorized');
-          expect(resfromdel.body.name).to.equal('UnauthorizedError');
+    // get /provinsi/search
+    it('should not get list of provinsi by searching if provinsi or higher is not logged in', done => {
+      chai
+        .request(routes)
+        .get('/api/provinsi/search')
+        .end((err, resfromget) => {
+          expect(err);
+          expect(resfromget).to.have.status(401);
+          expect(resfromget.body.message).to.equal('Not logged in.');
+          expect(resfromget.body.name).to.equal('UnauthorizedError');
           done();
         });
-      });
     });
 
-    it('should not edit user if user is not logged in', (done) => {
-      chai.request(routes).post('/api/users').send(createNewUser).end((err, res) => {
-        expect(err).to.be.falsy;
-        chai.request(routes).patch('/api/users/' + createNewUser.username).send({ nim: 13515074 }).end((errfromdel, resfromdel) => {
-          expect(err).to.be.falsy;
-          expect(resfromdel).to.have.status(401);
-          expect(resfromdel.body.message).to.equal('Unauthorized');
-          expect(resfromdel.body.name).to.equal('UnauthorizedError');
+    // get /provinsi/:username
+    it('should not get spesific kota if user is not logged in', done => {
+      chai
+        .request(routes)
+        .get('/api/provinsi/' + provinsi.username)
+        .end((err, resfromget) => {
+          expect(err);
+          expect(resfromget).to.have.status(401);
+          expect(resfromget.body.name).to.equal('UnauthorizedError');
           done();
         });
-      });
+    });
+
+    // patch /provinsi/:username
+    it('should not edit kota if kota or higher is not logged in', done => {
+      chai
+        .request(routes)
+        .patch('/api/provinsi/' + provinsi.username)
+        .send({ alamat: 'here' })
+        .end((err, resfromget) => {
+          expect(err);
+          expect(resfromget).to.have.status(401);
+          expect(resfromget.body.message).to.equal('Unauthorized');
+          expect(resfromget.body.name).to.equal('UnauthorizedError');
+          done();
+        });
     });
   });
 });
