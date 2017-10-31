bbe22c20d377240543d68fe75bffda905904c5d0
adrianhp97 <adrianhp100797@gmail.com>
2017-10-31 10:45:57 +0700

    add migration

---
+++ migrations/20171013182000_kategori.js
@@ -0,0 +1,14 @@
+exports.up = (knex, Promise) => {
+    return Promise.all([
+        knex.schema.createTable('kategori', table => {
+            table.int('id_kategori').primary();
+            table.string('nama_kategori');
+        })
+    ]);
+};
+
+exports.down = (knex, Promise) => {
+    return Promise.all([
+        knex.schema.dropTable('kategori')
+    ]);
+};
---
+++ migrations/20171013182222_layanan.js
@@ -0,0 +1,17 @@
+exports.up = (knex, Promise) => {
+    return Promise.all([
+        knex.schema.createTable('layanan', table => {
+            table.int('id_subkategori').primary();
+            table.int('id_subkategori').references('subkategori').onDelete('CASCADE');
+            table.string('nama_layanan');
+            table.string('verified');
+            table.datetime('tanggal_verified');
+        })
+    ]);
+};
+
+exports.down = (knex, Promise) => {
+    return Promise.all([
+        knex.schema.dropTable('layanan')
+    ]);
+};
