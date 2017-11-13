19060b7b536f172a1b56c751f78754cb7410e93b
adrianhp97 <13515091@std.stei.itb.ac.id>
2017-11-13 18:15:49 +0700

    add seed

--- seeds/2.users.1.js
+++ seeds/2.users.1.js
@@ -1,45 +1,10 @@
 exports.seed = (knex, Promise) => {
-  let provinsiQuery = () => {
-    return knex('user_provinsi')
-      .then(() => {
-        let users = [
-          {
-            username: 'sumatera_utara',
-            email: 'sumatera_utara_provinsi@e-gov.id',
-            password:
-              '$2a$08$QwNk.YBBpry2N09/LVudj.ZvVtkL.6JIYvGd8Y8MT9VQ3mMfHwF8S',
-            role: 'provinsi',
-            status: 'disabled'
-          } // Password: admin
-        ];
-        return Promise.all([
-          // Inserts seed entries
-          knex('users').insert(users)
-        ]);
-      })
-      .then(() => {
-        let provinsi = [
-          {
-            username: 'sumatera_utara',
-            nama: 'sumatera_utara',
-            nama_dinas: 'dinas_uksu',
-            kepala_dinas: 'Ray',
-            alamat: 'Jalan medan no.10'
-          }
-        ];
-        return Promise.all([
-          // Inserts seed entries
-          knex('user_provinsi').insert(provinsi)
-        ]);
-      });
-  };
-
   let kotaQuery = () => {
     return knex('user_kota')
       .then(() => {
         let users = [
           {
-            username: 'kota_medan',
+            username: 'kota_parapat',
             email: 'kota_medan@e-gov.id',
             password:
               '$2a$08$QwNk.YBBpry2N09/LVudj.ZvVtkL.6JIYvGd8Y8MT9VQ3mMfHwF8S',
@@ -55,12 +20,12 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let kota = [
           {
-            username: 'kota_medan',
+            username: 'kota_parapat',
             username_provinsi: 'sumatera_utara',
-            nama: 'medan',
-            nama_dinas: 'dinas_medan',
-            kepala_dinas: 'Ray',
-            alamat: 'Jalan medan no.10'
+            nama: 'parapat',
+            nama_dinas: 'dinas_parapat',
+            kepala_dinas: 'Verin',
+            alamat: 'Jalan parapat no.10'
           }
         ];
         return Promise.all([
@@ -75,8 +40,8 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let users = [
           {
-            username: 'puskesmas_medan',
-            email: 'puskesmas_medan@e-gov.id',
+            username: 'puskesmas_parapat',
+            email: 'puskesmas_parapat@e-gov.id',
             password:
               '$2a$08$QwNk.YBBpry2N09/LVudj.ZvVtkL.6JIYvGd8Y8MT9VQ3mMfHwF8S',
             role: 'puskesmas',
@@ -91,12 +56,12 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let puskesmas = [
           {
-            username: 'puskesmas_medan',
-            username_kota: 'kota_medan',
+            username: 'puskesmas_parapat',
+            username_kota: 'kota_parapat',
             nama: 'datafreaksPuskesmas',
-            nama_dians: 'dinas_puskesmas_medan',
-            kepala_dinas: 'Ray',
-            alamat: 'Jalan medan no.10'
+            nama_dians: 'dinas_puskesmas_parapat',
+            kepala_dinas: 'Verin',
+            alamat: 'Jalan parapat no.10'
           }
         ];
         return Promise.all([
@@ -111,8 +76,8 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let users = [
           {
-            username: 'kestrad_medan',
-            email: 'kestrad_medan@e-gov.id',
+            username: 'kestrad_parapat',
+            email: 'kestrad_parapat@e-gov.id',
             password:
               '$2a$08$QwNk.YBBpry2N09/LVudj.ZvVtkL.6JIYvGd8Y8MT9VQ3mMfHwF8S',
             role: 'kestrad',
@@ -127,13 +92,13 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let kestrad = [
           {
-            username: 'kestrad_medan',
-            username_puskesmas: 'puskesmas_medan',
+            username: 'kestrad_parapat',
+            username_puskesmas: 'puskesmas_parapat',
             nama: 'datafreaksKestrad',
-            penanggung_jawab: 'Ray',
-            jumlah_pegawai: 100,
-            alamat: 'Jalan medan no.10',
-            kecamatan: 'medan_kota'
+            penanggung_jawab: 'Verin',
+            jumlah_pegawai: 12,
+            alamat: 'Jalan parapat no.10',
+            kecamatan: 'parapat_kota'
           }
         ];
         return Promise.all([
@@ -148,7 +113,7 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let kategori = [
           {
-            nama_kategori: 'bagian_dalam'
+            nama_kategori: 'fisiologi'
           }
         ];
         return Promise.all([
@@ -164,7 +129,7 @@ exports.seed = (knex, Promise) => {
         let subkategori = [
           {
             id_kategori: 0,
-            nama_subkategori: 'bedah_otak'
+            nama_subkategori: 'gerakan_sehat'
           }
         ];
         return Promise.all([
@@ -180,8 +145,8 @@ exports.seed = (knex, Promise) => {
         let layanan = [
           {
             id_subkategori: 0,
-            username_kestrad: 'kestrad_medan',
-            nama_layanan: 'herbal',
+            username_kestrad: 'kestrad_parapat',
+            nama_layanan: 'tusuk',
             verified: 'menunggu'
           }
         ];
@@ -198,8 +163,8 @@ exports.seed = (knex, Promise) => {
         let hattra = [
           {
             id_layanan: 0,
-            nama_hattra: 'Aggi',
-            ijin_hattra: '151515'
+            nama_hattra: 'Aldrich',
+            ijin_hattra: '121512'
             verified: 'menunggu'
           }
         ];
@@ -210,27 +175,12 @@ exports.seed = (knex, Promise) => {
       })
   };
 
-  let provinsiCountQuery = () => {
-    return knex('user_provinsi_additional')
-      .then(() => {
-        let additional = [
-          {
-            username: 'sumatera_utara'
-          }
-        ];
-        return Promise.all([
-          // Inserts seed entries
-          knex('user_provinsi_additional').insert(additional)
-        ]);
-      })
-  };
-
   let kotaCountQuery = () => {
     return knex('user_kota_additional')
       .then(() => {
         let additional = [
           {
-            username: 'kota_medan'
+            username: 'kota_parapat'
           }
         ];
         return Promise.all([
@@ -245,7 +195,7 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let additional = [
           {
-            username: 'puskesmas_medan'
+            username: 'puskesmas_parapat'
           }
         ];
         return Promise.all([
@@ -260,7 +210,7 @@ exports.seed = (knex, Promise) => {
       .then(() => {
         let additional = [
           {
-            username: 'kestrad_medan'
+            username: 'kestrad_parapat'
           }
         ];
         return Promise.all([
@@ -271,7 +221,6 @@ exports.seed = (knex, Promise) => {
   };
 
   return knex('users')
-  .then(provinsiQuery)
   .then(kotaQuery)
   .then(puskesmasQuery)
   .then(kestradQuery)
@@ -279,7 +228,6 @@ exports.seed = (knex, Promise) => {
   .then(subkategoriQuery)
   .then(layananQuery)
   .then(hattraQuery)
-  .then(provinsiCountQuery)
   .then(kotaCountQuery)
   .then(puskesmasCountQuery)
   .then(kestradCountQuery);
