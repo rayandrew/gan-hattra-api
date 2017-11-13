a25776b07a19278af199c7867a54599e895242db
adrianhp97 <13515091@std.stei.itb.ac.id>
2017-11-13 17:47:01 +0700

    add seed

--- seeds/0.puskesmas.js
+++ seeds/0.puskesmas.js
@@ -1,4 +1,52 @@
 exports.seed = (knex, Promise) => {
+  let deleteQuery = () => {
+    return knex('hattra').del()
+      .then(() => {
+        return knex('layanan').del();
+      })
+      .then(() => {
+        return knex('subkategori').del();
+      })
+      .then(() => {
+        return knex('kategori').del();
+      })
+      .then(() => {
+        return knex('kestrad').del();
+      })
+      .then(() => {
+        return knex('puskesmas').del();
+      })
+      .then(() => {
+        return knex('kota').del();
+      })
+      .then(() => {
+        return knex('provinsi').del();
+      })
+      .then(() => {
+        return knex('user').del();
+      });
+  };
+  
+  let adminQuery = () => {
+     return knex('users')
+    .then(() => {
+      let users = [
+        {
+          username: 'admin',
+          email: 'admin@e-gov.id',
+          password:
+            '$2y$10$3ApPdLQHjB.i69PVYPyV3eMXT/qjkKNKo4grgkkRk8AOGm7RA/Xvy',
+          role: 'admin',
+          status: 'active'
+        } // Password: admin
+      ];
+      return Promise.all([
+        // Inserts seed entries
+        knex('users').insert(users)
+      ]);
+    });
+  };
+
   let provinsiQuery = () => {
     return knex('user_provinsi')
       .then(() => {
@@ -22,6 +70,7 @@ exports.seed = (knex, Promise) => {
           {
             username: 'jawa_barat_provinsi',
             nama: 'jawa_barat',
+            nama_dinas: 'dinas_jabar',
             kepala_dinas: 'Adrian',
             alamat: 'Jalan Ranggagempol no.10'
           }
@@ -57,6 +106,7 @@ exports.seed = (knex, Promise) => {
             username: 'kota_tasik',
             username_provinsi: 'jawa_barat',
             nama: 'tasik',
+            nama_dinas: 'dinas_tasik',
             kepala_dinas: 'Adrian',
             alamat: 'Jalan Ranggagempol no.10'
           }
@@ -92,6 +142,7 @@ exports.seed = (knex, Promise) => {
             username: 'puskesmas_tasik',
             username_kota: 'tasik',
             nama: 'datafreaksPuskesmas',
+            nama_dians: 'dinas_puskesmas_tasik',
             kepala_dinas: 'Adrian',
             alamat: 'Jalan Ranggagempol no.10'
           }
@@ -103,9 +154,119 @@ exports.seed = (knex, Promise) => {
       });
   };
 
+  let kestradQuery = () => {
+    return knex('user_kestrad')
+      .then(() => {
+        let users = [
+          {
+            username: 'kestrad_tasik',
+            email: 'kestrad_tasik@e-gov.id',
+            password:
+              '$2a$08$QwNk.YBBpry2N09/LVudj.ZvVtkL.6JIYvGd8Y8MT9VQ3mMfHwF8S',
+            role: 'kestrad',
+            status: 'disabled'
+          } // Password: admin
+        ];
+        return Promise.all([
+          // Inserts seed entries
+          knex('users').insert(users)
+        ]);
+      })
+      .then(() => {
+        let kestrad = [
+          {
+            username: 'kestrad_tasik',
+            username_puskesmas: 'puskesmas_tasik',
+            nama: 'datafreaksKestrad',
+            penanggung_jawab: 'Adrian',
+            jumlah_pegawai: 50,
+            alamat: 'Jalan Ranggagempol no.10',
+            kecamatan: 'Tawang'
+          }
+        ];
+        return Promise.all([
+          // Inserts seed entries
+          knex('user_kestrad').insert(kestrad)
+        ]);
+      });
+  };
+
+  let kategoriQuery = () => {
+    return knex('kategori')
+      .then(() => {
+        let kategori = [
+          {
+            nama_kategori: 'pelajaran'
+          }
+        ];
+        return Promise.all([
+          // Inserts seed entries
+          knex('kategori').insert(kategori)
+        ]);
+      })
+  };
+
+  let subkategoriQuery = () => {
+    return knex('kategori')
+      .then(() => {
+        let subkategori = [
+          {
+            id_kategori: 0,
+            nama_subkategori: 'pelajaran_olahraga'
+          }
+        ];
+        return Promise.all([
+          // Inserts seed entries
+          knex('subkategori').insert(subkategori)
+        ]);
+      })
+  };
+
+  let layananQuery = () => {
+    return knex('layanan')
+      .then(() => {
+        let layanan = [
+          {
+            id_subkategori: 0,
+            username_kestrad: 'kestrad_tasik',
+            nama_layanan: 'akupuntur',
+            verified: 'menunggu'
+          }
+        ];
+        return Promise.all([
+          // Inserts seed entries
+          knex('layanan').insert(layanan)
+        ]);
+      })
+  };
+
+  let hattraQuery = () => {
+    return knex('hattra')
+      .then(() => {
+        let hattra = [
+          {
+            id_layanan: 0,
+            nama_hattra: 'Ray',
+            ijin_hattra: '123456'
+            verified: 'menunggu'
+          }
+        ];
+        return Promise.all([
+          // Inserts seed entries
+          knex('hattra').insert(hattra)
+        ]);
+      })
+  };
+
   return knex('users')
-    .del()
+    .then(deleteQuery)
+    .then(adminQuery)
     .then(provinsiQuery)
     .then(kotaQuery)
-    .then(puskesmasQuery);
+    .then(puskesmasQuery)
+    .then(kestradQuery)
+    .then(kategoriQuery)
+    .then(subkategoriQuery)
+    .then(layananQuery)
+    .then(hattraQuery);
 };
